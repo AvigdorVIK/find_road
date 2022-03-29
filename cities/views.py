9515abadfee0fe905e16ccbf72b4554c7879db88
AvigdorVIK <kanynnikovvik19@gmail.com>
2022-03-29 15:18:48 +0300

    work with templates and criate 'base.html'

--- cities/views.py
+++ cities/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
 
+from cities.models import City
+
+
 def home (request):
-    return render (request, 'cities/home.html')
+    qs = City.objects.all()
+    context = {'list_name': qs}
+
+
+    return render (request, 'cities/home.html', context)
