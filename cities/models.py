ea2ae6ca59f86f7f6fde0706c1fa1e0f1682475a
AvigdorVIK <kanynnikovvik19@gmail.com>
2022-03-31 12:24:40 +0300

    add bootstrap

--- cities/models.py
+++ cities/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 class City(models.Model):
     name = models.CharField(max_length=100, unique=True)
+    description = models.CharField(max_length= 500, null=True)
+
 
     def __str__(self):
         return self.name
